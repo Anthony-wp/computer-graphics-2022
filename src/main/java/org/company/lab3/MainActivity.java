6e924354bb958863639321dac8cfa7f0ea04f6e6
Anthony-wp <66904574+Anthony-wp@users.noreply.github.com>
2022-04-17 12:34:02 +0300

    initial commit

---
+++ src/main/java/org/company/lab3/MainActivity.java
@@ -0,0 +1,18 @@
+//package org.company.lab3;
+//
+//import android.app.Activity;
+//import android.os.Bundle;
+//public class MainActivity extends Activity { // наследуем от Activity
+//    /** Called when the activity is first created. */
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState); // вызываем функцию onCreate класса-родителя
+//        Drawer d = new Drawer(this); // создаем экземпляр нашего класса
+//        setContentView(d); // и устанавливаем отображение нашего класса
+//        //setContentView(R.layout.main);
+//    }
+//
+//    public static void main(String[] args) {
+//        MainActivity mainActivity = new MainActivity();
+//    }
+//}
